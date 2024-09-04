c7945234ba6b4413ad59c2ca25280072acbbc4c6
Victor <carvajal57685@gmail.com>
2024-09-04 09:49:23 -0500

    Conexión con la base de datos

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import { PORT } from './config.js';
+import db from './database/db.js';
 
 
 const app = express();
@@ -7,7 +9,19 @@ const app = express();
 app.use( cors() );
 app.use( express.json() );
 
+try {
 
-app.listen( 3000, () => {
-    console.log('server online en el puerto 3000');    
+    db.authenticate()
+    console.log('Conexión exitosa con la base de datos');
+
+} catch (error) {
+    
+    console.log( error );
+    console.log('Error al conectar con la base de datos');
+    
+}
+
+
+app.listen( PORT, () => {
+    console.log(`server online en el puerto ${PORT}`);    
 })
