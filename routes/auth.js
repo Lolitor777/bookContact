ae260d73309143abcf1524938cb8c443700e7ebc
Victor <carvajal57685@gmail.com>
2024-09-04 13:37:10 -0500

    Creación del login sin token de autenticacion

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { createUser } from '../controllers/auth.js';
+import { createUser, login } from '../controllers/auth.js';
 
 const router = Router();
 
 router.post( '/crearUsuario', createUser );
-
+router.post( '/login', login ); 
 
 
 export default router;
